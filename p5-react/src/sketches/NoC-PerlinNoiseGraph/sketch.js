6cc0ff309f36f7528ee2ed4cfdb6c942aaea4129
Gabe Koss <gabe@gabekoss.com>
2019-01-20 03:06:43 -0500

    make the noise sketch less brutal to load (still slow af)

--- p5-react/src/sketches/NoC-PerlinNoiseGraph/sketch.js
+++ p5-react/src/sketches/NoC-PerlinNoiseGraph/sketch.js
@@ -3,19 +3,12 @@ export default function (p) {
     let xSize = 500;
     let ySize = 250;
     p.createCanvas(xSize*2, ySize);
-    for (var i = 0; i < p.width; i++) {
-      for (var j = 0; j < p.height; j++) {
-        let stroke = p.map(p.noise(i*0.01,j*0.01), 0, 1, 0, 255);
-        p.stroke(stroke);
+    for (var i = 0; i < xSize; i++) {
+      for (var j = 0; j < ySize; j++) {
+        let val = Math.floor(p.map(p.noise(i*0.01,j*0.01), 0, 1, 0, 255))
+        p.stroke(val);
         p.point(i, j)
-      }
-    }
-    for (var i = 0; i < p.width; i++) {
-      for (var j = 0; j < p.height; j++) {
-        let stroke = p.color('hsb(' + Math.floor(p.map(p.noise(i*0.01,j*0.01), 0, 1, 0, 255)) +', 100%, 100%)')
-
-        // console.log(stroke.levels)
-        p.stroke(stroke);
+        p.stroke(p.color('hsb(' + val +', 65%, 75%)'));
         p.point(i+xSize, j)
       }
     }
