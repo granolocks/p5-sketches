28b3215ded9248b60291f1418d6f7be1f118430d
Gabe Koss <gabe@gabekoss.com>
2019-01-26 20:20:46 -0500

    some more variations

--- p5-react/src/sketches/12-MandelbrotChords/sketch3.js
+++ p5-react/src/sketches/12-MandelbrotChords/sketch3.js
@@ -1,9 +1,9 @@
 export default function (p) {
   p.totalLines = 200;
-  p.multiplier = 2;
+  p.multiplier = 0;
   p.pts = [];
   p.radius = 290;
-  p.current = 0;
+  // p.current = 0;
   p.lines = [];
 
   p.calcConnection = function(i) {
@@ -18,6 +18,7 @@ export default function (p) {
 
   p.setup = function() {
     p.createCanvas(600, 600);
+    p.frameRate(60);
     p.background(0);
     p.ellipseMode(p.CENTER)
     p.colorMode(p.HSB, 100);
@@ -37,25 +38,34 @@ export default function (p) {
     p.stroke(255)
     p.ellipse(0,0,p.radius*2,p.radius*2)
     p.fill(255)
+    //
+    // if (p.current >= p.totalLines) {
+    //   p.current = 0;
+    //   p.multiplier += 0.087
+    // }
 
-    if (p.current < p.totalLines) {
-      let p1 = p.pts[p.current]
-      let p2theta = p.TWO_PI / p.totalLines * p.calcConnection(p.current)
+    for (var i = 0; i < p.totalLines; i++ ) {
+      let p1 = p.pts[i]
+      let p2theta = p.TWO_PI / p.totalLines * p.calcConnection(i)
       let p2 = p.polarToCartesian(p.radius,p2theta);
-      p.lines[p.current] = [p1, p2]
-
-      for (var i = 0; i < p.lines.length; i++ ) {
-        let h = p.map(i, 0, p.totalLines, 0, 100)
-        p.stroke(h, 100, 50)
-        let pp1 = p.lines[i][0]
-        let pp2 = p.lines[i][1]
-        p.line(pp1.x, pp1.y, pp2.x, pp2.y)
-      }
-
-      p.current++
-    } else {
-      p.current = 0;
-      p.multiplier += 0.1
+      p.lines[i] = [p1, p2]
     }
+
+    for (var i = 0; i < p.lines.length; i++ ) {
+      let h = p.map(i, 0, p.totalLines, 0, 100)
+      let w = p.map(i, 0, p.totalLines, 0, 3)
+      p.stroke(h, 100, 50)
+      let pp1 = p.lines[i][0]
+      let pp2 = p.lines[i][1]
+      p.line(pp1.x, pp1.y, pp2.x, pp2.y)
+
+    }
+
+
+    for (var i = 0; i < p.totalLines-1; i++) {
+      let xy = p.polarToCartesian(p.radius, p.TWO_PI / p.totalLines * i)
+      p.ellipse(xy.x,xy.y,3,3)
+    }
+    p.multiplier += 0.0087
   }
 }
